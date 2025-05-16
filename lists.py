d7bf7b6ddcdd35d2d746659246a51ccfd214d3f1
piagarciaf <pgarciafernandez@mail.austral.edu.ar>
2025-05-15 21:27:11 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,31 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    lista = list_to_remove_elements[:]
+    if len(lista) >5:
+        del lista[5]
+    if len(lista) >4:
+        del lista[4]
+    if len(lista) >0:
+        del lista[0]
+    return lista
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    list_to_add_elements.insert(0,"Pink")
+    list_to_add_elements.append("Yellow")
+    return list_to_add_elements
+print(add_elements(['Red', 'Green', 'White', 'Black']))
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+        return len(list_to_check) ==0
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if len(list_to_compare1) >=3 and len(list_to_compare2) >=3:
+        return list_to_compare1[2] == list_to_compare2[2]
+    else: 
+        return False
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    a = list_of_lists_to_modify[0][:2]
+    b = list_of_lists_to_modify[1][1:4]
+    c = list_of_lists_to_modify[2][-2:]
+    return [a, b, c]
